1adeb5170f12691603dc7b894dc7c62b4cd419cd
Win Builder <windev@epsitec.ch>
2014-04-25 16:06:09 +0200

    Fix the way how the variables are set with the config file.

--- pkg/install.bat
+++ pkg/install.bat
@@ -1,6 +1,6 @@
 @echo off
 
-for /f "delims=" %%x in ("packages\%1\config") do (set "%%x")
+for /f "delims=" %%x in (packages\%1\config) do (set "%%x")
 
 echo [%1] install
 call packages\%1\install
