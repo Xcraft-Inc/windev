7f9a2dafd402def08b6cbf7d4144011f4a4013d5
Mathieu Schroeter <schroeter@epsitec.ch>
2014-05-08 14:20:03 +0200

    Fix the path for 7zip.

--- zip/7zip.bat
+++ zip/7zip.bat
@@ -6,9 +6,8 @@ for %%F in ("%0") do set dirname=%%~dpF
 pushd %dirname%
 
 setlocal
-set PATH="%path%;C:\Program Files\7-Zip"
+set PATH=C:\Program Files\7-Zip;%PATH%
 7z.exe x -o%2 %1
 endlocal
 
 popd
-
