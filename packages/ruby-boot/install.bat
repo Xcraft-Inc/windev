26b018ec0fd26f3507869785792c2537252dccb3
Mathieu Schroeter <schroeter@epsitec.ch>
2014-05-08 14:25:02 +0200

    We use 7zip as decomp.bat script.

--- packages/ruby-boot/install.bat
+++ packages/ruby-boot/install.bat
@@ -1,3 +1,3 @@
 @echo off
 
-call zip\7zip %pkg_dst% %pkg_sys%
+call 7zip\decomp %pkg_dst% %pkg_sys%
--- zip/decomp.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set dirname=
-for %%F in ("%0") do set dirname=%%~dpF
-
-pushd %dirname%
-cscript //B decomp.vbs "%1" "%2"
-popd
