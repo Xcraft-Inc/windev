c9e27aee9145d2dfe645b11b940adc24eeda8307
Mathieu Schroeter <schroeter@epsitec.ch>
2014-05-08 14:19:47 +0200

    Set the MAKEFLAGS with the number of logical processors - 1.

--- pkg/install.bat
+++ pkg/install.bat
@@ -3,5 +3,8 @@
 for /f "delims=" %%x in (pkg\config) do (set "%%x")
 for /f "delims=" %%x in (packages\%1\config) do (set "%%x")
 
+set /a NPROC=%NUMBER_OF_PROCESSORS%-1
+set MAKEFLAGS=-j%NPROC%
+
 echo [%1] install
 call packages\%1\install
