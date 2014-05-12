877e9a3dd92baca6b0b1f22f550b0073f92f4acb
Mathieu Schroeter <schroeter@epsitec.ch>
2014-05-12 17:01:11 +0200

    Disable the SSL verify step.

--- packages/toolchain/get.bat
+++ packages/toolchain/get.bat
@@ -4,6 +4,7 @@ setlocal
 echo Enter your git.epsitec.ch username:
 set /p username=
 
+git config --global http.sslVerify false
 git config --global core.askpass "C:\Program Files (x86)\Git\libexec\git-core\git-gui--askpass"
 
 git clone https://%username%@%pkg_src% %pkg_dst%
