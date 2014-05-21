6c0ff4d63acb38ace6f4e6d72b7d13a96821cca1
Mathieu Schroeter <schroeter@epsitec.ch>
2014-05-21 14:22:03 +0200

    Add outputs for begin and end of stage0.

--- bootstrap-nodejs.bat
+++ bootstrap-nodejs.bat
@@ -1,5 +1,7 @@
 @echo off
 
+echo [stage0] begin of stage0
+
 call pkg/uninstall nodejs
 call pkg/remove sysroot
 call pkg/install sysroot
@@ -9,4 +11,5 @@ call pkg/install nodejs
 
 call pkg/get toolchain
 
+echo [stage0] end of stage0
 call node %boot_sysroot%\toolchain\scripts\stage1.js
