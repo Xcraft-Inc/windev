7d4bc6c7a27fa12a0a715691d376648f380c278a
Mathieu Schroeter <mathieu@schroetersa.ch>
2014-06-03 14:25:06 +0200

    Fixes #1, our nodejs should be the first.

--- packages/nodejs/install.bat
+++ packages/nodejs/install.bat
@@ -4,4 +4,4 @@ msiexec /i %pkg_dst% INSTALLDIR="%boot_sysroot%\nodejs" /qb
 del /Q %pkg_dst%
 
 where node >NUL 2>&1
-if errorlevel 1 set PATH=%PATH%;%boot_sysroot%\nodejs
+if errorlevel 1 set PATH=%boot_sysroot%\nodejs;%PATH%
