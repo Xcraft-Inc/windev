86ea4a5cad91cfb72b7e92ea2e78ca7c59a515ea
Mathieu Schroeter <mathieu@schroetersa.ch>
2014-06-03 16:54:48 +0200

    Use a better variable name.

--- packages/toolchain/get.bat
+++ packages/toolchain/get.bat
@@ -2,10 +2,10 @@
 
 setlocal
 echo Enter your git.epsitec.ch username:
-set /p username=
+set /p location=
 
 git config --global http.sslVerify false
 git config --global core.askpass "C:\Program Files (x86)\Git\libexec\git-core\git-gui--askpass"
 
-git clone https://%username%@%pkg_src% %pkg_dst%
+git clone https://%location%@%pkg_src% %pkg_dst%
 endlocal
