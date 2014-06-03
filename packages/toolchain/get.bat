6871e84274e4390ea24b17c7443e2402144179b5
Mathieu Schroeter <mathieu@schroetersa.ch>
2014-06-03 17:06:38 +0200

    Add the support for tollchain downloading with windev.epsitec.local.

--- packages/toolchain/get.bat
+++ packages/toolchain/get.bat
@@ -1,11 +1,16 @@
 @echo off
 
 setlocal
-echo Enter your git.epsitec.ch username:
-set /p location=
 
 git config --global http.sslVerify false
 git config --global core.askpass "C:\Program Files (x86)\Git\libexec\git-core\git-gui--askpass"
+
+if x%USERDOMAIN%==xWINDEV (
+  set location=gitlab-ci-token:%pkg_ci_token%
+) else (
+  echo Enter your git.epsitec.ch username:
+  set /p location=
+)
 
 git clone https://%location%@%pkg_src% %pkg_dst%
 endlocal
