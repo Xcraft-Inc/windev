39e0019dbe8d556ac816cb0e89f50f88f48623ba
Mathieu Schroeter <mathieu@schroetersa.ch>
2014-08-03 19:38:23 +0200

    Install msys.

--- packages/mingw-get/install.bat
+++ packages/mingw-get/install.bat
@@ -3,4 +3,4 @@
 call 7zip\decomp %pkg_dst% %boot_sysroot%
 del /Q %pkg_dst%
 
-%boot_sysroot%\bin\mingw-get install gcc g++ mingw32-make
+%boot_sysroot%\bin\mingw-get install gcc g++ mingw32-make msys
