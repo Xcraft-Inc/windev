bd8f58fe3f7fc7c9225ab0bff08ddc9566c9e347
Mathieu Schroeter <schroeter@epsitec.ch>
2014-08-04 09:33:20 +0200

    Add the mounting point for mingw. Note that it is very important to ensure that the sysroot path have no space.

--- packages/mingw-get/install.bat
+++ packages/mingw-get/install.bat
@@ -4,3 +4,4 @@ call 7zip\decomp %pkg_dst% %boot_sysroot%
 del /Q %pkg_dst%
 
 %boot_sysroot%\bin\mingw-get install gcc g++ mingw32-make msys
+echo %boot_sysroot% /mingw >%boot_sysroot%\msys\1.0\etc\fstab
