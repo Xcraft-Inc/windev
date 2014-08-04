36ea0500f1e26eb7250c6830c0da7bc1db6666bd
Mathieu Schroeter <schroeter@epsitec.ch>
2014-08-04 10:38:34 +0200

    Add mingw32-libz and mingw32-libgmp which are needed in order to build cmake.

--- packages/mingw-get/install.bat
+++ packages/mingw-get/install.bat
@@ -3,5 +3,5 @@
 call 7zip\decomp %pkg_dst% %boot_sysroot%
 del /Q %pkg_dst%
 
-%boot_sysroot%\bin\mingw-get install gcc g++ mingw32-make msys
+%boot_sysroot%\bin\mingw-get install gcc g++ mingw32-make mingw32-libz mingw32-libgmp msys
 echo %boot_sysroot% /mingw >%boot_sysroot%\msys\1.0\etc\fstab
