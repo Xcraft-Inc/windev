d187150123514adc60fb28ae324de22eb64369b0
Mathieu Schroeter <schroeter@epsitec.ch>
2014-08-08 15:52:47 +0200

    Then mingw-get is only used in order to have msys.

--- packages/mingw-get/install.bat
+++ packages/mingw-get/install.bat
@@ -3,5 +3,5 @@
 call 7zip\decomp %pkg_dst% %boot_sysroot%
 del /Q %pkg_dst%
 
-%boot_sysroot%\bin\mingw-get install gcc g++ mingw32-make mingw32-libz mingw32-libgmp msys msys-patch
+%boot_sysroot%\bin\mingw-get install msys msys-patch
 echo %boot_sysroot% /mingw >%boot_sysroot%\msys\1.0\etc\fstab
