595af9105c45031940561366a7d8bd53c61b239f
Mathieu Schroeter <schroeter@epsitec.ch>
2014-08-08 15:04:48 +0200

    Add a package mingw64 (not used).

---
+++ packages/mingw64/install.bat
@@ -0,0 +1,10 @@
+@echo off
+
+call 7zip\decomp %pkg_dst% %boot_sysroot%
+del /Q %pkg_dst%
+
+SET src_folder=%boot_sysroot%\mingw64
+SET tar_folder=%boot_sysroot%
+
+for /f %%a IN ('dir "%src_folder%" /b') do move /Y %src_folder%\%%a %tar_folder%
+rmdir %boot_sysroot%\mingw64
