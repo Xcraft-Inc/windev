cc0d5fec5c79df2959a469e81781d54075532163
Mathieu Schroeter <schroeter@epsitec.ch>
2014-08-14 12:32:33 +0200

    The rmdir command is bugged, errorlevel was not set correctly.

--- packages/nodejs-bin/uninstall.bat
+++ packages/nodejs-bin/uninstall.bat
@@ -1,4 +1,4 @@
 @echo off
 
-rd /Q /S "%boot_sysroot%/opt/nodejs"
-if [%errorlevel%]==3 exit /b 0
+rd /Q /S "%boot_sysroot%/opt/nodejs" || rem
+if [%errorlevel%]==[2] exit /b 0
--- packages/sysroot/remove.bat
+++ packages/sysroot/remove.bat
@@ -1,6 +1,10 @@
 @echo off
 
-rd /Q /S %boot_sysroot%
-if not [%errorlevel%]==3 exit /b
-rd /Q /S %boot_build%
-if [%errorlevel%]==3 exit /b 0
+rd /Q /S %boot_sysroot% || rem
+if [%errorlevel%]==[0] goto next
+if [%errorlevel%]==[2] goto next
+exit /b
+
+:next
+rd /Q /S %boot_build% || rem
+if [errorlevel]==[2] exit /b 0
