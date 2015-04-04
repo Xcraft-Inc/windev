8667a32fe9c6909f3cd2f469d1cc42eed3d0e9f7
Mathieu Schroeter <mathieu@schroetersa.ch>
2015-04-04 11:17:22 +0200

    Fix the case where 'find' comes from msysgit.
    It closes issue #9.

--- packages/nodejs-bin/env.bat
+++ packages/nodejs-bin/env.bat
@@ -2,6 +2,13 @@
 
 set pattern=%boot_sysroot%\opt\nodejs
 
-echo ;%PATH%; | find /C /I ";%pattern%;"
+rem Look for the Windows find binary.
+set FIND=
+for /f %%i in ('where find') do (
+  "%%i" /? >NUL 2>&1
+  if errorlevel 0 set FIND=%%i
+)
+
+echo ;%PATH%; | %FIND% /C /I ";%pattern%;"
 if errorlevel 1 set PATH=%boot_sysroot%\opt\nodejs;%PATH%
 exit /b 0
