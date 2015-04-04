aa2fc6d5011d359a389c1dd9014cb5131a77f872
Mathieu Schroeter <mathieu@schroetersa.ch>
2015-04-04 09:53:16 +0200

    Fix the delete of sysroot/ by using utils/rmd.

--- packages/sysroot/remove.bat
+++ packages/sysroot/remove.bat
@@ -1,12 +1,11 @@
 @echo off
 
-rd /Q /S %boot_sysroot% || rem
-if [%errorlevel%]==[0] goto next
-if [%errorlevel%]==[2] goto next
-if [%errorlevel%]==[3] goto next
+call utils\rmd %boot_build% || rem
+if errorlevel 0 goto next
+if errorlevel 2 goto next
 exit /b
 
 :next
-rd /Q /S %boot_build% || rem
-if [%errorlevel%]==[2] exit /b 0
-if [%errorlevel%]==[3] exit /b 0
+call utils\rmd %boot_sysroot% || rem
+if errorlevel 2 exit /b 0
+exit /b
