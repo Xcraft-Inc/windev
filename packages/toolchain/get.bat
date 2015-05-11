86164bd7f265464252d93c1a1c9bb1f629ee463e
Mathieu Schroeter <schroeter@epsitec.ch>
2015-05-11 14:36:07 +0200

    Remove Windev CI exception (obsolete).

--- packages/toolchain/get.bat
+++ packages/toolchain/get.bat
@@ -8,12 +8,8 @@ git config --global http.sslVerify false
 git config --global core.askpass "C:\Program Files (x86)\Git\libexec\git-core\git-gui--askpass"
 
 :connect_
-if x%USERDOMAIN%==xWINDEV (
-  set location=gitlab-ci-token:%pkg_ci_token%
-) else (
-  echo Enter your git.epsitec.ch username:
-  set /p location=
-)
+echo Enter your git.epsitec.ch username:
+set /p location=
 
 git clone https://%location%@%pkg_src% %pkg_dst%
 if not [%errorlevel%]==[0] goto connect_
