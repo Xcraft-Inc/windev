12635422295897acbef663535789c5b7d24b3194
Roger Vuistiner <vuistiner@epsitec.ch>
2018-08-06 16:28:11 +0200

    Search for curl in the user installation of git

--- utils/web/get.bat
+++ utils/web/get.bat
@@ -6,7 +6,7 @@ for %%F in ("%0") do set dirname=%%~dpF
 pushd %dirname%
 
 setlocal
-set PATH=C:\Program Files\Git\mingw64\bin;C:\Program Files (x86)\Git\mingw32\bin;C:\Program Files (x86)\Git\bin;%PATH%
+set PATH=%LOCALAPPDATA%\Programs\Git\mingw64\bin;C:\Program Files\Git\mingw64\bin;C:\Program Files (x86)\Git\mingw32\bin;C:\Program Files (x86)\Git\bin;%PATH%
 
 curl -L -o %2 %1
 endlocal
