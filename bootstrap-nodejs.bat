e8332f6a991376ed656f3f2e13bd12a3c5aa7048
Mathieu Schroeter <mathieu@schroetersa.ch>
2021-03-15 15:52:45 +0100

    Replace old msys/mingw by msys2

--- bootstrap-nodejs.bat
+++ bootstrap-nodejs.bat
@@ -12,11 +12,8 @@ call pkg/remove 7zip || exit /b
 call pkg/get 7zip || exit /b
 call pkg/install 7zip || exit /b
 
-call pkg/get mingw64 || exit /b
-call pkg/install mingw64 || exit /b
-
-call pkg/get msys || exit /b
-call pkg/install msys || exit /b
+call pkg/get msys2 || exit /b
+call pkg/install msys2 || exit /b
 
 call pkg/remove jsmsix || exit /b
 call pkg/get jsmsix || exit /b
@@ -36,7 +33,7 @@ if exist "%cmake_path%" set boot_cmake=%ProgramFiles(x86)%\CMake\bin
 set cmake_path=%ProgramFiles%\CMake\bin\cmake.exe
 if exist "%cmake_path%" set boot_cmake=%ProgramFiles(x86)%\CMake\bin
 
-call node %boot_sysroot%\toolchain\lib\xcraft\stage1.js "%boot_cmake%" "%boot_sysroot%\bin" "%boot_sysroot%\opt\nodejs" "%boot_sysroot%\opt\bin" "%boot_sysroot%\msys\1.0\bin"
+call node %boot_sysroot%\toolchain\lib\xcraft\stage1.js "%boot_cmake%" "%boot_sysroot%\bin" "%boot_sysroot%\opt\nodejs" "%boot_sysroot%\opt\bin" "%boot_sysroot%\opt\msys64\usr\bin" "%boot_sysroot%\opt\msys64\mingw64\bin"
 
 cd %boot_sysroot%\toolchain\
 exit /b
@@ -46,8 +43,7 @@ exit /b
 echo Building a dist package
 
 call pkg/cache 7zip || exit /b
-call pkg/cache mingw64 || exit /b
-call pkg/cache msys || exit /b
+call pkg/cache msys2 || exit /b
 call pkg/cache jsmsix || exit /b
 call pkg/cache nodejs-bin || exit /b
 call pkg/cache toolchain || exit /b
---
+++ packages/msys2/install.bat
@@ -0,0 +1,8 @@
+@echo off
+
+call utils\7zip\decomp %pkg_dst% %boot_sysroot%\msys2
+mkdir "%boot_sysroot%\opt
+call move %boot_sysroot%\msys2\msys64 %boot_sysroot%\opt\msys64
+
+del /Q %pkg_dst%
+rmdir /Q %boot_sysroot%\msys2
