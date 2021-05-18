ea3f7d61064f36f3e8b88791f614032f22813ebf
Mathieu Schroeter <schroeter@epsitec.ch>
2021-05-18 09:58:29 +0000

    Try to unzip with PowerShell if it fails with Shell.Application

--- packages/7zip/install.bat
+++ packages/7zip/install.bat
@@ -1,12 +1,17 @@
 @echo off
 
-echo strZipFile = "%pkg_dst%" > %boot_build%/unzip.vbs
-echo outFolder = "%boot_build%\7zip" >> %boot_build%/unzip.vbs
-echo Set objShell = CreateObject( "Shell.Application" ) >> %boot_build%/unzip.vbs
+echo On Error Resume Next                                    > %boot_build%/unzip.vbs
+echo strZipFile = "%pkg_dst%"                               >> %boot_build%/unzip.vbs
+echo outFolder = "%boot_build%\7zip"                        >> %boot_build%/unzip.vbs
+echo Set objShell = CreateObject( "Shell.Application" )     >> %boot_build%/unzip.vbs
 echo Set objSource = objShell.NameSpace(strZipFile).Items() >> %boot_build%/unzip.vbs
-echo Set objTarget = objShell.NameSpace(outFolder) >> %boot_build%/unzip.vbs
-echo intOptions = 256 >> %boot_build%/unzip.vbs
-echo objTarget.CopyHere objSource, intOptions >> %boot_build%/unzip.vbs
+echo Set objTarget = objShell.NameSpace(outFolder)          >> %boot_build%/unzip.vbs
+echo intOptions = 256                                       >> %boot_build%/unzip.vbs
+echo objTarget.CopyHere objSource, intOptions               >> %boot_build%/unzip.vbs
+echo If Err.Number <> 0 Then                                >> %boot_build%/unzip.vbs
+echo   Set objShell = CreateObject("Wscript.shell")         >> %boot_build%/unzip.vbs
+echo   objShell.Run("powershell Expand-Archive -LiteralPath " & strZipFile & "-DestinationPath " & outFolder) >> %boot_build%/unzip.vbs
+echo End If
 
 mkdir %boot_build%\7zip
 call wscript %boot_build%/unzip.vbs
