46e0c720563ebc7b34019233268b4f2fdcdd4681
Mathieu Schroeter <schroeter@epsitec.ch>
2021-06-25 10:40:17 +0000

    Remove obsolete settings for git

--- packages/xcraft/get.bat
+++ packages/xcraft/get.bat
@@ -4,9 +4,6 @@ if exist %pkg_cache% goto cache_
 
 setlocal
 
-git config --global http.sslVerify false
-git config --global core.askpass "C:\Program Files (x86)\Git\libexec\git-core\git-gui--askpass"
-
 :connect_
 echo Enter your git.epsitec.ch username:
 set /p location=
