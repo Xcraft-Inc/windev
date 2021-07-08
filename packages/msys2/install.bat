72b8e0e124253a49ec5488b6b15fb703272df6bc
Mathieu Schroeter <mathieu@schroetersa.ch>
2021-07-08 10:14:13 +0200

    Fix typo

--- packages/msys2/install.bat
+++ packages/msys2/install.bat
@@ -1,7 +1,7 @@
 @echo off
 
 call utils\7zip\decomp %pkg_dst% %boot_sysroot%\msys2
-mkdir "%boot_sysroot%\opt
+mkdir %boot_sysroot%\opt
 call move %boot_sysroot%\msys2\msys64 %boot_sysroot%\opt\msys64
 
 del /Q %pkg_dst%
