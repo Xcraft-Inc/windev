a8335b20d41ffd2342cde27f9714051d072c4cbd
Mathieu Schroeter <schroeter@epsitec.ch>
2025-08-06 17:25:17 +0200

    Upgrade node to the latest v22 and npm to v11

--- packages/nodejs-bin/install.bat
+++ packages/nodejs-bin/install.bat
@@ -4,7 +4,7 @@ call utils\7zip\decomp %pkg_dst% %boot_sysroot%\nodejs
 mkdir %boot_sysroot%\opt
 call move %boot_sysroot%\nodejs\node-%pkg_ver%-win-x64 %boot_sysroot%\opt\nodejs
 
-call %boot_sysroot%\opt\nodejs\node.exe %boot_sysroot%\opt\nodejs\node_modules\npm\bin\npm-cli.js install --prefix=%boot_sysroot%\opt\nodejs npm@10.5.2
+call %boot_sysroot%\opt\nodejs\node.exe %boot_sysroot%\opt\nodejs\node_modules\npm\bin\npm-cli.js install --prefix=%boot_sysroot%\opt\nodejs npm@11.5.2
 
 del /Q %pkg_dst%
 rmdir /Q %boot_sysroot%\nodejs
