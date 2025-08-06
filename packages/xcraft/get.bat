7e6e061936b64c64ca339a404a771d81bcca6fb2
Mathieu Schroeter <schroeter@epsitec.ch>
2025-08-06 17:26:07 +0200

    Ask for Xcraft Git host and path (use github by default)

--- packages/xcraft/get.bat
+++ packages/xcraft/get.bat
@@ -4,16 +4,37 @@ if exist %pkg_cache% goto cache_
 
 setlocal
 
+if [%pkg_host%]==[] (
+  echo Choose a Git host for Xcraft ^(default: github.com^)
+  echo For Epsitec SA, provide git.epsitec.ch
+  set /p pkg_host=
+  if [%pkg_host%]==[] (
+    set pkg_host=github.com
+    set pkg_path=Xcraft-Inc/xcraft.git
+  )
+)
+
+if [%pkg_path%]==[] (
+  echo Choose a Git path for Xcraft ^(default: Xcraft-Inc/xcraft.git^)
+  echo For Epsitec SA, provide xcraft/xcraft.git
+  set /p pkg_path=
+  if [%pkg_path%]==[] (
+    set pkg_path=Xcraft-Inc/xcraft.git
+  )
+)
+
+set pkg_src=%pkg_host%/%pkg_path%
+
 git remote get-url origin | findstr https://
 if [%errorlevel%]==[0] (
   :connect_
   echo Enter your git.epsitec.ch username:
-  set /p location=
+  set /p login=
 
-  git clone --recursive https://%location%@%pkg_host%/%pkg_src% %pkg_dst%
+  git clone --recursive https://%login%@%pkg_src% %pkg_dst%
   if not [%errorlevel%]==[0] goto connect_
 ) else (
-  git clone --recursive git@%pkg_host%:%pkg_src% %pkg_dst%
+  git clone --recursive git@%pkg_host%:%pkg_path% %pkg_dst%
 )
 
 endlocal
